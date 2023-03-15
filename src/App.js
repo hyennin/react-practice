66588bcf4bf81b799c2d87069ade8c9751474c11
hyennin <chm050202@gmail.com>
2023-03-15 14:01:04 +0900

    react 기초

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = undefined;
-  return <div>{name || '리액트'}</div>;
+  const name = '리액트';
+  return (
+    <>
+      <div className="react">{name || '리액트'}</div>
+      <input/>
+    </>
+  );
 } 
 
 export default App;
