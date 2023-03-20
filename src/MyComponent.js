351ec454713ab1f78205d64081661141a7ab34a4
hyennin <chm050202@gmail.com>
2023-03-20 14:32:48 +0900

    props 실습

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,29 @@
-const MyComponent = () => {
-    return <div>나의 새롭고 멋진 컴포넌트</div>
-}
+import PropTypes from "prop-types";
 
+const MyComponent = ({name, children, favoriteNumber}) => {
+    // name과 children을 속성값으로 설정
+    return (
+     <div> 안녕하세요, 제 이름은 {name}입니다. <br/>
+     childern 값은 {children} 
+     입니다.
+     <br/>
+     제가 좋아하는 숫자는 {favoriteNumber}입니다.
+     </div>
+    )
+};
+
+MyComponent.defaultProps = {
+    name: "기본이름"
+};
+
+MyComponent.propTypes = {
+    name: PropTypes.string
+    // 이렇게 설정하면 무조건 문자열 형태로 전달해야 한다.
+};
+
+MyComponent.propTypes = {
+    favoriteNumber: PropTypes.number.isRequired
+    // favoriteNumber 숫자 전달
+    // isRequired는 반드시 값을 넘겨주어야 한다.
+};
 export default MyComponent;
