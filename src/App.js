98be343e6ed650abe812068ef47fec27021a50d5
hyennin <chm050202@gmail.com>
2023-03-22 13:58:09 +0900

    useState 실습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Counter from './Counter';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
 import { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <Counter/>
+    return <Say/>
   }
 } 
 
