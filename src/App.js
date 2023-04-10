02be4f2f647ac58c0741c2a36c11bd5980d395c2
hyennin <chm050202@gmail.com>
2023-04-10 14:45:46 +0900

    error 처리 실습

--- src/App.js
+++ src/App.js
@@ -8,13 +8,34 @@ import ValidationSample from './ValidationSample';
 import RefSample from './RefSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
-class App extends Component{
+//랜덤 색상 생성
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  //16777215를 hex로 표현하면 ffffff가 되므로 000000 ~ ffffff 값 반환
+}
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+    color: getRandomColor(),
+    });
+  };
+  
   render() {
-    return(
-        <IterationSample/>
-    )
-  }
-} 
+    return (
+    <div>
+      <button onClick={this.handleClick}>랜덤 색상</button>
+      <ErrorBoundary>
+        <LifeCycleSample color={this.state.color} />
+      </ErrorBoundary>
+    </div>
+    );
+  } }
 
 export default App;
